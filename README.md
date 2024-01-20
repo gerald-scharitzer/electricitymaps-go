3921599add88ae42a45429402a9e846b8fcbd61d
gerald-scharitzer <gerald.scharitzer@gmail.com>
2024-01-20 21:26:51 +0100

    v0

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# electricitymaps-go
-Go client for the Electricity Maps API
+# Electricity Maps Go
+
+is a Go client for the [Electricity Maps API](https://static.electricitymaps.com/api/docs/index.html) as package `gopkg.in/electricitymaps`.
---
+++ health_test.go
@@ -0,0 +1,15 @@
+package electricitymaps
+
+import (
+	"net/http"
+	"testing"
+
+	"gotest.tools/v3/assert"
+)
+
+func TestGetHealth(t *testing.T) {
+	want := http.StatusOK
+	got, err := GetHealth()
+	assert.NilError(t, err)
+	assert.Equal(t, got, want)
+}
