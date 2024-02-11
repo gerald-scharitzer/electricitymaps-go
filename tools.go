1f20f81dde403cca9f5c221975059c5ecdb58858
gerald-scharitzer <gerald.scharitzer@gmail.com>
2024-02-11 17:09:50 +0100

    addr

--- tools.go
+++ tools.go
@@ -1,6 +1,6 @@
 package electromap
 
-// Get the address of a value as expression
+// Get the address of a non-addressable value as expression
 //
 //	a := addr(value)
 //
@@ -9,7 +9,39 @@ package electromap
 //	v := value
 //	a := &v
 //
-// The argument is passed by value, so the address of the copy is returned.
+// This is for constant and literal values,
+// because the [address operator](https://go.dev/ref/spec#Address_operators)
+// is defined for addressable operands only.
+//
+//	func f(number *int) {
+//	  fmt.Println(*number)
+//	}
+//
+// There is no automatic boxing in Go,
+// so function arguments are not referenced automatically.
+//
+//	const c = 42
+//	// f(c) // error: cannot use c (untyped int constant 42) as *int value in argument to f compiler(IncompatibleAssign)
+//
+//	v := c
+//	// f(v) // error: cannot use v (variable of type int) as *int value in argument to f compiler(IncompatibleAssign)
+//
+//	// f(42) // error: cannot use 42 (untyped int constant) as *int value in argument to f compiler(IncompatibleAssign)
+//
+// Instead the address of a function argument must be obtained explicitly,
+// but the address operator is defined for addressable operands only.
+//
+//	// f(&c) // invalid operation: cannot take address of c (untyped int constant 42) compiler(UnaddressableOperand)
+//	f(&v) // requires declaration of variable v
+//	// f(&42) // invalid operation: cannot take address of 42 (untyped int constant) compiler(UnaddressableOperand)
+//
+// The function `addr` allocates a variable implicitly and returns its address.
+//
+//	f(addr(c))
+//	f(addr(42))
+//	f(addr(v)) // is less efficient than f(&v) and points to the same value, but a different variable
+//
+// The argument is passed by value, so the address of the copy is returned instead of the original argument.
 // The memory is allocated implicitly through pass by value.
 func addr[T any](x T) *T {
 	return &x
